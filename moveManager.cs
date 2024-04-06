7ca12e2426dbe238d12edd44b80fbe70708d34c1
ClaudeDumas <140992823+ClaudeDumas@users.noreply.github.com>
2024-04-05 20:29:26 -0500

    Add files via upload

--- moveManager.cs
+++ moveManager.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class moveManager : MonoBehaviour
 {
 
-    [SerializeField] float frictionMultiplier;
+    [SerializeField] float frictionMultiplier = 1;
     public Vector3 Vel;
-    public float playerSpeed;
-    public float gravVal;
+    [SerializeField] float speed = 3;
+    public float gravVal = -9.81f;
     public float areaDensity = 1.22f;
     
-    public float frictionCoefficient = 1f;
+    public float frictionCoefficient = 1;
     [SerializeField] float dragCoefficient = .59f;
     [SerializeField] float mass = 30;
 
@@ -19,21 +19,23 @@ public class moveManager : MonoBehaviour
     void Update()
     {
         CharacterController charCol = GetComponent<CharacterController>();
+        // check if slow enough to stop, which is when calculating friction would make it negative.
+        if (Mathf.Min(Vel.magnitude, gravVal * frictionCoefficient * -1 * Time.deltaTime) == Vel.magnitude){
+            Vel -= Vel;
+        }
+        else{
+            // uses the actual friction formula, F = μN
+            Vel -= Vel.normalized * gravVal * frictionCoefficient * frictionMultiplier * -1 * Time.deltaTime;
+        }
+        // air resistance
+        Vel -= (((areaDensity * (Vel * Vel.magnitude) * (charCol.height * charCol.radius) * dragCoefficient)/2)/mass) * Time.deltaTime;
         // check for terminal velocity then apply gravity and air resistance
         if (Vel.y > -33)
         {
-            Vel.y += (gravVal * Time.deltaTime);
-            // air resistance
-            Vel.y -= (((areaDensity * Vel.y * Vel.y * (charCol.height * charCol.radius) * dragCoefficient)/2)/mass) * Time.deltaTime;
+            // applying gravity
+            Vel.y += (gravVal * Time.deltaTime * 2);
         }
-        if (Mathf.Min(Vel.magnitude, gravVal * frictionCoefficient * -1 * Time.deltaTime) == Vel.magnitude){
-            Vel -= Vel;
-        }else{
-            Vel -= Vel.normalized * gravVal * frictionCoefficient * frictionMultiplier * -1 * Time.deltaTime;
-        }
-        charCol.Move(Vel * Time.deltaTime * playerSpeed);
-        // reseting gravity in case jetpack was used and no longer is.
-        gravVal = -9.81f;
+        charCol.Move(Vel * Time.deltaTime * speed);
         // Make sure velocity doesn't go too far in the negatives while on the ground.
         if (charCol.isGrounded && Vel.y < 0){
             Vel.y = 0;
